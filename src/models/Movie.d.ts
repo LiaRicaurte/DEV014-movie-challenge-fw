cd45b46cb5d1a456d9737f15cb60fb66aee7f71b
LiaRicaurte <liaricaurte.arq@gmail.com>
2024-07-17 09:37:50 -0500

    agrega el archivo Movie.d.ts en la carpeta models para almacenar el tipo Movie

---
+++ src/models/Movie.d.ts
@@ -0,0 +1,10 @@
+export interface Movie {
+    id: number;
+    title: string;
+    releaseDate: string;
+    posterUrl: string;
+    genres: string[];
+    runTime: string;
+    overview: string;
+    rating: number;
+}
